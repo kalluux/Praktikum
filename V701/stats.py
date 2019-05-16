a397854f5cb277cbb8d73bf7675aea935f78511c
Daniel Wall <d.daniel.wall@gmail.com>
2019-05-16 20:12:48 +0200

    v701 fix

---
+++ V701/stats.py
@@ -0,0 +1,29 @@
+from scipy import stats
+import numpy as np
+import matplotlib.pylab as plt
+ 
+b = np.genfromtxt('n.txt', unpack=True)
+ 
+mean = np.mean(b)
+#sem = sem(b)
+std = np.std(b)
+var = std**2
+#print(mean,'+-','Varianz=',var)
+ 
+ 
+plt.hist(b, bins=10, density = True, label="Messwerte")
+xt = plt.xticks()[0]
+xmin, xmax = min(xt), max(xt)
+lnspc = np.linspace(xmin, xmax, len(b))
+
+
+pdf_g = stats.norm.pdf(lnspc, mean, std)
+plt.plot(lnspc, pdf_g, color="r", label="Gaußverteilung")
+plt.xlabel(r'Impulse / 1/10s')
+plt.ylabel(r'Relative Häufigkeit')
+ 
+poi = np.random.poisson(lam=mean, size= 10000)
+plt.hist(poi, bins=10, density = True, histtype = "step", color ="k", label="Poissonverteilung")
+plt.grid()
+plt.legend()
+plt.show()
